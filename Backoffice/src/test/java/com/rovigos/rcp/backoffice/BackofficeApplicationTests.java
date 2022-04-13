3dba9f6997601cc34b9a8692f47d0876d3d6e049
rovigos-minwoo <minwoo.cho@rovigos.com>
2022-04-13 07:24:28 +0000

    first commig

---
+++ Backoffice/src/test/java/com/rovigos/rcp/backoffice/BackofficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rovigos.rcp.backoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackofficeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
